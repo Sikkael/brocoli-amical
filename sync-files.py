d3404bbc0921c383ed09aa1cca3ee26ee3ad36ed
Sikkael <Mouellet@live.ca>
2025-03-03 15:51:53 -0500

    Choosing the best approach

--- sync-files.py
+++ sync-files.py
@@ -24,19 +24,14 @@ def check_directories(src_dir, dst_dir):
 # Function to synchronize files between two directories
 def sync_directories(src_dir, dst_dir, delete=False):
     # Get a list of all files and directories in the source directory
-    files_to_sync = []
-    for root, dirs, files in os.walk(src_dir):
-        log(f"root-> {root}, dirs-> {dirs}, files-> {files}")
-        for directory in dirs:
-            files_to_sync.append(os.path.join(root, directory))
-        for file in files:
-            files_to_sync.append(os.path.join(root, file))
+    files_to_sync = list_directories(src_dir)
 
     # Iterate over each file in the source directory with a progress bar
     with tqdm(total=len(files_to_sync), desc="Syncing files", unit="file") as pbar:
         # Iterate over each file in the source directory
         for source_path in files_to_sync:
             # Get the corresponding path in the replica directory
+
             replica_path = os.path.join(dst_dir, os.path.relpath(source_path, src_dir))
 
             # Check if path is a directory and create it in the replica directory if it does not exist
@@ -62,26 +57,35 @@ def sync_directories(src_dir, dst_dir, delete=False):
 
             # Update the progress bar
             pbar.update(1)
+
+
 # Listing directories, subdirectories and files
 def list_directories(_dir):
-    files_to_copy = []
+    files_to_sync = []
     for root, dirs, files in os.walk(_dir):
         for directory in dirs:
-            files_to_copy.append(os.path.join(root, directory))
+            files_to_sync.append(os.path.join(root, directory))
         for file in files:
-            files_to_copy.append(os.path.join(root, file))
-    return files_to_copy
+            files_to_sync.append(os.path.join(root, file))
+    return files_to_sync
 
 
 def clean_backup(src_dir, dst_dir):
     pass
 
 if __name__ == "__main__":
-    src_pth = "C:\\"
+    src_pths = ["C:\\Blockchain","C:\\C++", "C:\\CocosCreator",
+                "C:\\codelite-amd64-12.0.8", "C:\\codelite-amd64-12.0.9"
+                , "C:\\CodeLiteProjects", "C:\\data","C:\\cpp","C:\\cpp-ethereum"
+                , "C:\\backup"]
 
     dst_pth = "F:\\sauvegarde-mike"
-    check_directories(src_pth,dst_pth)
-    sync_directories(src_pth, dst_pth)
+
+    for src in src_pths:
+        log(src, "src_pth.log")
+        print(os.path.join(dst_pth, os.path.relpath(src, "C:\\")))
+        check_directories(src, os.path.join(dst_pth, os.path.relpath(src, "C:\\")))
+        sync_directories(src, os.path.join(dst_pth, os.path.relpath(src, "C:\\")))
 
 #dr_lst = list_directories("C:\\Users")
 
